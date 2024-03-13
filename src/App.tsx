be85fe2806b7093e259b13d550a4bb017b26969d
rathoreshivesh <rathoreshivesh22@gmail.com>
2024-03-14 02:58:07 +0530

    :sparkles: add progressbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-import CapturePage from './pages/DocumentsUpload/CameraCapture'
-
+import "./App.css";
+// import CapturePage from "./pages/DocumentsUpload/CameraCapture";
+import Progress from "./components/ProgressBar";
 export default function App() {
   return (
     <div className="App">
-      <CapturePage />
+      <Progress />
     </div>
-  )
+  );
 }
