71f08487be2dfa379209b5a42ce299fdbaf86542
ironnicko <nickonicko779@gmail.com>
2024-03-15 13:03:21 +0530

    split string fixed

--- src/components/OCR/ocr.ts
+++ src/components/OCR/ocr.ts
@@ -1,51 +1,51 @@
 import Tesseract from 'tesseract.js';
 
-const mult: number[][] = [
-  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [1, 2, 3, 4, 0, 6, 7, 8, 9, 5],
-  [2, 3, 4, 0, 1, 7, 8, 9, 5, 6],
-  [3, 4, 0, 1, 2, 8, 9, 5, 6, 7],
-  [4, 0, 1, 2, 3, 9, 5, 6, 7, 8],
-  [5, 9, 8, 7, 6, 0, 4, 3, 2, 1],
-  [6, 5, 9, 8, 7, 1, 0, 4, 3, 2],
-  [7, 6, 5, 9, 8, 2, 1, 0, 4, 3],
-  [8, 7, 6, 5, 9, 3, 2, 1, 0, 4],
-  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-];
+class Verhoeff {
+  private static readonly d: number[][] = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 0, 6, 7, 8, 9, 5],
+    [2, 3, 4, 0, 1, 7, 8, 9, 5, 6],
+    [3, 4, 0, 1, 2, 8, 9, 5, 6, 7],
+    [4, 0, 1, 2, 3, 9, 5, 6, 7, 8],
+    [5, 9, 8, 7, 6, 0, 4, 3, 2, 1],
+    [6, 5, 9, 8, 7, 1, 0, 4, 3, 2],
+    [7, 6, 5, 9, 8, 2, 1, 0, 4, 3],
+    [8, 7, 6, 5, 9, 3, 2, 1, 0, 4],
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+  ];
 
-const perm: number[][] = [
-  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [1, 5, 7, 6, 2, 8, 3, 0, 9, 4],
-  [5, 8, 0, 3, 7, 9, 6, 1, 4, 2],
-  [8, 9, 1, 6, 0, 4, 3, 5, 2, 7],
-  [9, 4, 5, 3, 1, 2, 6, 8, 7, 0],
-  [4, 2, 8, 6, 5, 7, 3, 9, 0, 1],
-  [2, 7, 9, 3, 8, 0, 6, 4, 1, 5],
-  [7, 0, 4, 6, 9, 1, 3, 2, 5, 8]
-];
+  private static readonly p: number[][] = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 5, 7, 6, 2, 8, 3, 0, 9, 4],
+    [5, 8, 0, 3, 7, 9, 6, 1, 4, 2],
+    [8, 9, 1, 6, 0, 4, 3, 5, 2, 7],
+    [9, 4, 5, 3, 1, 2, 6, 8, 7, 0],
+    [4, 2, 8, 6, 5, 7, 3, 9, 0, 1],
+    [2, 7, 9, 3, 8, 0, 6, 4, 1, 5],
+    [7, 0, 4, 6, 9, 1, 3, 2, 5, 8]
+  ];
 
+  static validateAadhar(aadharNum: string): boolean {
+    console.log("Hi");
+    try {
+      let checksum = 0;
+      var newAadhar : string[] = aadharNum.split(" ");
+      var newNum = newAadhar.join("");
+      aadharNum = newNum;
+      let len = aadharNum.length;
 
-const checkAadhar = (text : any)=>{
-  try {
-    let i = text.length;
-    let j = 0;
-    let x = 0;
-
-    while (i > 0) {
-        i -= 1;
-        x = mult[x][perm[(j % 8)][parseInt(text[i])]];
-        j += 1;
+      for (let i = 0; i < len; i++) {
+        let index = parseInt(aadharNum.charAt(i));
+        let row = len - i;
+        checksum = Verhoeff.d[checksum][Verhoeff.p[row % 8][index]];
+      }
+      return checksum === 0;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-    if (x === 0) {
-        return true;
-    } else {
-        return false;
-    }
-} catch (error) {
-    return false;
+  }
 }
-};
-
 const extractTextAndSearchPattern = (imagePath, option) => {
 
   const patterns = {
@@ -62,18 +62,17 @@ const extractTextAndSearchPattern = (imagePath, option) => {
         logger: m => console.log(m)
       }
     ).then(({ data: { text } }) => {
-      console.log(text);
       const search = text.match(pattern);
 
       if (search) {
-        if (option == "aadhar"){
-          if (checkAadhar(search[0]))
+        if (option == "aadhar") {
+          if (Verhoeff.validateAadhar(search[0]))
             resolve(search[0]);
           else
             reject("Aadhar Number was not verifiable");
         }
-        else(option == 'pan')
-          resolve(search[0]);
+        else (option == 'pan')
+        resolve(search[0]);
       } else {
         reject(`No ${option} number found`);
       }
